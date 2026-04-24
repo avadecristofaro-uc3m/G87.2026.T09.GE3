6e8856f64cf35a7d72740776a303b7c74f251bdb
avadecristofaro-uc3m <100591007@alumnos.uc3m.es>
2026-04-24 16:23:07 +0200

    resolved pylint errors for DescriptionAttribute

--- src/main/python/uc3m_consulting/attribute/description_attribute.py
+++ src/main/python/uc3m_consulting/attribute/description_attribute.py
@@ -1,13 +1,14 @@
 """Class for validating description"""
-from .attribute import Attribute
+from uc3m_consulting.attribute.attribute import Attribute
 
 class DescriptionAttribute(Attribute):
     """Class for description attribute"""
     def __init__(self, attr_value):
-        self.validation_pattern = r"^.{10,30}$"
+        super().__init__()
         self.error_message = "Invalid description format"
-        super().validate(attr_value)
+        self.validation_pattern = r"^.{10,30}$"
         self._attr_value = attr_value
+        super().validate(attr_value)
 
     @property
     def value(self):
