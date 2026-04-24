d66c0a713e632aab1eb21845cecbad0fe9757ec5
100591002 <100591002@alumnos.uc3m.es>
2026-04-24 22:53:23 +0200

    made documents json store singleton

--- src/main/python/uc3m_consulting/json_storage/documents_json_store.py
+++ src/main/python/uc3m_consulting/json_storage/documents_json_store.py
@@ -1,23 +1,36 @@
 """Module for managing the Json Documents store"""
 
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
 from uc3m_consulting.json_storage.json_store import JsonStore
 from uc3m_consulting.enterprise_manager_config import TEST_DOCUMENTS_STORE_FILE
 
+class DocumentsJsonStore:
+    """Singleton Documents JSON store"""
+    class _DocumentsJsonStore(JsonStore):
+        """Handles persistence of documents"""
 
-class DocumentsJsonStore(JsonStore):
-    """Handles persistence of documents"""
+        _FILE_PATH = TEST_DOCUMENTS_STORE_FILE
 
-    _FILE_PATH = TEST_DOCUMENTS_STORE_FILE
+        def load(self):
+            """Load documents"""
+            return super().load_json_file()
+
+        def save(self):
+            """Save all documents"""
+            super().save_json_file()
+
+        def save_documents(self, documents_list):
+            """Replace all documents and persist"""
+            self._data_list = documents_list
+            self.save()
+
+    __instance = None
+
+    def __new__(cls):
+        if not DocumentsJsonStore.__instance:
+            DocumentsJsonStore.__instance = DocumentsJsonStore._DocumentsJsonStore()
+        return DocumentsJsonStore.__instance
 
     def load(self):
         """Load documents"""
-        return super().load_json_file()
-
-    def save(self):
-        """Save all documents"""
-        super().save_json_file()
-
-    def save_documents(self, documents_list):
-        """Replace all documents and persist"""
-        self._data_list = documents_list
-        self.save()
+        return self.__instance.load()
