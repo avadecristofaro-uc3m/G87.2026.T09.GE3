cce295ca398fc5f753ee0ee67fc98a82d90ce644
100591002 <100591002@alumnos.uc3m.es>
2026-04-24 23:03:16 +0200

    made num docs store singleton

--- src/main/python/uc3m_consulting/json_storage/num_docs_json_store.py
+++ src/main/python/uc3m_consulting/json_storage/num_docs_json_store.py
@@ -3,21 +3,35 @@
 from uc3m_consulting.json_storage.json_store import JsonStore
 from uc3m_consulting.enterprise_manager_config import TEST_NUMDOCS_STORE_FILE
 
+class NumDocsJsonStore:
+    """Singleton JSON store for document count reports."""
 
-class NumDocsJsonStore(JsonStore):
-    """Handles persistence of document count reports"""
 
-    _FILE_PATH = TEST_NUMDOCS_STORE_FILE
+    class _NumDocsJsonStore(JsonStore):
+        """Handles persistence of document count reports"""
 
-    def load(self):
-        """Load reports"""
-        return super().load_json_file()
+        _FILE_PATH = TEST_NUMDOCS_STORE_FILE
 
-    def save(self):
-        """Save all reports"""
-        super().save_json_file()
+        def load(self):
+            """Load reports"""
+            return super().load_json_file()
 
-    def save_reports(self, reports_list):
-        """Replace all reports and persist"""
-        self._data_list = reports_list
-        self.save()
+        def save(self):
+            """Save all reports"""
+            super().save_json_file()
+
+        def save_reports(self, reports_list):
+            """Replace all reports and persist"""
+            self._data_list = reports_list
+            self.save()
+
+    __instance = None
+
+    def __new__(cls):
+        if not NumDocsJsonStore.__instance:
+            NumDocsJsonStore.__instance = NumDocsJsonStore._NumDocsJsonStore()
+        return NumDocsJsonStore.__instance
+
+    def add_to_store(self, new_item: dict):
+        """Add a new report to the store."""
+        return self.__instance.add_to_store(new_item)
