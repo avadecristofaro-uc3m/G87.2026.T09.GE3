a16425e8fa5b3de4910cf553564f37f4738ca1bb
100591002 <100591002@alumnos.uc3m.es>
2026-04-24 22:46:14 +0200

    added tests for project json store

--- src/unittest/python/test_singletons_tests.py
+++ src/unittest/python/test_singletons_tests.py
@@ -13,6 +13,13 @@ class SingletonTest(TestCase):
         self.assertEqual(enterprise_manager1, enterprise_manager2)  # assert instances are the same
         self.assertEqual(enterprise_manager2, enterprise_manager3)  # assert instances are the same
 
+    def test_projects_json_store_singleton(self):
+        """Validate that projects JSON store class is instantiated as a Singleton"""
+        projects_store1 = ProjectsJsonStore()
+        projects_store2 = ProjectsJsonStore()
+        projects_store3 = ProjectsJsonStore()
+        self.assertEqual(projects_store1, projects_store2)
+        self.assertEqual(projects_store2, projects_store3)
 
 # if __name__ == '__main__':
 #     unittest.main()
